2acc33a62ba39f429aec61eaf7cc546360845595
buddypall1 <buddypall321@gmail.com>
2024-12-13 22:47:31 +0100

    Completed WPS upgrades
    Also added info about current problems and upcoming updates

--- EOC.py
+++ EOC.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import pygame
 import pickle
 from numerize import numerize
+from tkinter import messagebox
 
 Wonderhoys = 0
 wps = 0
@@ -10,6 +11,9 @@ clickpower = 1
 # WPS values
 firstupgrpay = 10
 scndupgrpay = 1000
+thrdupgrpay = 10000
+frthupgrpay = 1000000
+
 # Click values
 firstclickupgrpay = 10
 
@@ -27,11 +31,14 @@ def load_data():
         return data
     except (FileNotFoundError, EOFError, pickle.UnpicklingError):
         print("Error loading data or file not found. Initializing default values.")
+        messagebox.showerror('Data Error', "Data file has been deleted or tampered with. Resetting progress..")
         return {
             "Wonderhoys": 0,
             "wps": 0,
             "firstupgrpay": 10,
             "scndupgrpay": 1000,
+            "thrdupgrpay": 10000,
+            "frthupgrpay": 1000000,
             "firstclickupgrpay": 10,
             "clickpower": 1,
         }
@@ -42,6 +49,8 @@ def save_data():
         "wps": wps,
         "firstupgrpay": firstupgrpay,
         "scndupgrpay": scndupgrpay,
+        "thrdupgrpay": thrdupgrpay,
+        "frthupgrpay": frthupgrpay,
         "firstclickupgrpay": firstclickupgrpay,
         "clickpower": clickpower,
     }
@@ -54,8 +63,11 @@ Wonderhoys = game_data["Wonderhoys"]
 wps = game_data["wps"]
 firstupgrpay = game_data["firstupgrpay"]
 scndupgrpay = game_data["scndupgrpay"]
+thrdupgrpay = game_data["thrdupgrpay"]
+frthupgrpay = game_data["frthupgrpay"]
 firstclickupgrpay = game_data["firstclickupgrpay"]
 clickpower = game_data["clickpower"]
+
 
 def on_closing():
     save_data()
@@ -107,6 +119,31 @@ def scndupgr():
     else:
         pass
 
+def thrdupgr():
+    global Wonderhoys, wps, thrdupgrpay
+    if Wonderhoys >= thrdupgrpay:
+        Wonderhoys -= thrdupgrpay
+        thrdupgrpay += thrdupgrpay + 10
+        update_wonderhoy_label()
+        wps += 15
+        wpslabel.config(text=f"WPS: {wps}")
+        print(wps)
+    else:
+        pass
+
+def frthupgrr():
+    global Wonderhoys, wps, frthupgrpay
+    if Wonderhoys >= frthupgrpay:
+        Wonderhoys -= frthupgrpay
+        frthupgrpay += frthupgrpay + 20
+        update_wonderhoy_label()
+        wps += 15
+        wpslabel.config(text=f"WPS: {wps}")
+        print(wps)
+    else:
+        pass
+
+
 # Click Upgrades
 def cfrstupg():
     global Wonderhoys, clickpower, firstclickupgrpay
@@ -120,13 +157,23 @@ def cfrstupg():
         
 def update_wonderhoy_label():
     global Wonderhoys, wps,clickpower,firstupgrpay, scndupgrpay
+    # WPS upgrade numerize
     readable_wonderhoys = numerize.numerize(Wonderhoys)
     readable_wpsupg1 = numerize.numerize(firstupgrpay)
     readable_wpsupg2 = numerize.numerize(scndupgrpay)
+    readable_wpsupg3 = numerize.numerize(thrdupgrpay)
+    readable_wpsupg4 = numerize.numerize(frthupgrpay)
+    # click upgrade numerize
+    readable_clckupg1 = numerize.numerize(firstclickupgrpay)
     Wonderhoyammount.config(text=f"Wonderhoys:\n{readable_wonderhoys}")
     # WPS upgrade labels
-    upgrade1.config(text=f"Emu Helper\n\n{readable_wpsupg1} WH\n\nadds 2 WPS")
-    upgrade2.config(text=f"Tsukasa Helper\n\n{readable_wpsupg2} WH\n\nadds 5 WPS")
+    upgrade1.config(text=f"Emu Helper\n\n{readable_wpsupg1} WH\n\nAdds 2 WPS")
+    upgrade2.config(text=f"Tsukasa Helper\n\n{readable_wpsupg2} WH\n\nAdds 5 WPS")
+    upgrade3.config(text=f"Nene Helper\n\n{readable_wpsupg3} WH\n\nAdds 50 WPS")
+    upgrade4.config(text=f"Rui Helper\n\n{readable_wpsupg4} WH\n\nAdds 100 WPS")
+    # click upgrade labels
+    cpsupgrade1.config(text=f"Miku Helper\n\n {readable_clckupg1} WH\n\nAdds 2 Click Power")
+
 
 
 def addwonderhoys():
@@ -166,9 +213,9 @@ emuchibi = Label(window, image=emuchibiparent, background="Pink")
 emuchibi.place(x=624, y=185)
 upgrade2 = Button(window, text=f"Tsukasa Helper\n{scndupgrpay} WH\nadds 5 WPS", command=scndupgr, bg="pink", relief='groove', fg="#e236be", font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
 upgrade2.place(x=740, y=320)
-upgrade3 = Button(window, text=f"Dummytext\ntext", bg="pink", relief='groove', fg="#e236be", font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
+upgrade3 = Button(window, text=f"Nene Helper\n{thrdupgrpay} WH\n\nadds 15 WPS", command=thrdupgr, bg="pink", relief='groove', fg="#e236be", font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
 upgrade3.place(x=740, y=440)
-upgrade4 = Button(window, text=f"Dummytext\ntext", bg="pink", relief='groove', fg="#e236be", font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
+upgrade4 = Button(window, text=f"Rui Helper\ntext", bg="pink", relief='groove', fg="#e236be", command=frthupgrr, font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
 upgrade4.place(x=740, y=560)
 
 # CPS Upgrades GUI
@@ -182,6 +229,14 @@ cpsupgrade3 = Button(window, command=firstupgr, text=f"CPSupgr\ntext", bg="pink"
 cpsupgrade3.place(x=27, y=440)
 cpsupgrade4 = Button(window, command=firstupgr, text=f"CPSupgr\ntext", bg="pink", relief='groove', fg="#e236be", font=("Arial", 10, "bold"), borderwidth=5, width=20, height=5)
 cpsupgrade4.place(x=27, y=560)
+# Misc GUI
+noninf = Label(window, text= "Version: NonInf!", background= "Pink", font= ("arial", 10, "bold"))
+noninf.place(y= 10)
+version = Label(window, text= "Game Currently\nbreaks formatting\nafter Trillion WH's.\n(still playable,\nhowever hard to read.)", background="pink", font= ("arial", 10, "bold"))
+version.place(x=35, y=10)
+nextup = Label(window, text= "Next up:\n InfVersion.\nBetter Upgrades", background="pink", font= ("Arial", 10, "bold"))
+nextup.place(x=777, y=10)
+
 
 
 def center_label(label, window_width, y_position):
@@ -190,11 +245,14 @@ def center_label(label, window_width, y_position):
     x_position = (window_width - label_width) // 2  
     label.place(x=x_position, y=y_position)
 
+    
 
 window_width = 950  # width of window
 
 
 center_label(Wonderhoyammount, window_width, 50)
+
+center_label(noninf, window_width, 10)
 
 center_label(wpslabel, window_width, 160)
 
