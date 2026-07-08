4ecec1feebdc3433472dc6fad5affee489d87a6d
buddypall1 <buddypall321@gmail.com>
2026-07-08 21:27:14 +0200

    basic shop menu prototype

--- EOC.py
+++ EOC.py
@@ -1,9 +1,11 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QScrollArea, QWidget, QVBoxLayout, QStackedWidget, QMessageBox, QFrame
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QScrollArea, QWidget, QVBoxLayout, QStackedWidget, QMessageBox, QFrame, QHBoxLayout
 from PySide6.QtCore import Qt, QTimer, QSize
 from PySide6.QtGui import QFont, QIcon, QPixmap
 import pygame
 import pickle
+
+pygame.mixer.init()
 
 saveloc = "Data/game_data.dat"
 
@@ -55,7 +57,7 @@ window = QMainWindow()
 mainwidget = QWidget()
 mainwidget.setObjectName("background")
 window.setCentralWidget(mainwidget)
-window.setFixedSize(1920,1080)
+window.setFixedSize(1910,1000)
 
 pixmap = QPixmap("Images\other\emu.png").scaled(350,350, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
 
@@ -68,11 +70,17 @@ mainwidget.setStyleSheet("""
 layout = QVBoxLayout()
 mainwidget.setLayout(layout)
 
+wondahoy = pygame.mixer.Sound("SFX/WONDERHOY SOUND EFFECT (no background music).mp3")
+wondahoy.set_volume(0.05)
+
 def clickevent():
     global wonderhoys, money
 
     wonderhoys += 1
     money.setText(f"Wonderhoys: {wonderhoys}")
+    wondahoy.play(loops=0)
+
+
 
 
 emubutton = QPushButton()
@@ -93,11 +101,12 @@ emubutton.setStyleSheet("""
         padding-top: 4px;
     }
 """)
+
 emubutton.setParent(mainwidget)
 
 def centerer(input, xoffset, yoffset):
-    x = (1920 - input.width()) // 2
-    y = (1080 - input.height()) // 2
+    x = (1910 - input.width()) // 2
+    y = (1000 - input.height()) // 2
     input.move(x + xoffset,y + yoffset)
 
 centerer(emubutton, 0, -100)
@@ -120,9 +129,114 @@ money.setStyleSheet("""
     }
 """)
 
+shopmainwidget = QWidget()
+shopmainlayout = QVBoxLayout()
+shopmainwidget.setLayout(shopmainlayout)
+shopmainwidget.resize(400,1000)
+shopmainwidget.setParent(mainwidget)
+shopmainwidget.setObjectName("Test")
+shopmainwidget.setStyleSheet("""
+    #Test {
+    background-color: #fce0ed;
+    color: #ffb2d7;
+    border: 4px solid #ffb2d7;   
+    border-radius: 4px;
+    }
+""")
+shoptitle = QLabel("Shop!")
+shoptitle.setAlignment(Qt.AlignCenter)
+shoptitle.resize(200,10)
+shoptitle.setObjectName("Shopname")
+shoptitle.setStyleSheet("""
+    #Shopname{
+    background-color: #ffb8ce;
+    color: #ff7aa4;
+    border: 5px solid #ff7aa4;   
+    border-radius: 4px;
+    font-size: 65px;
+    font-weight: bold;
+    }
+""")
+shopmainlayout.addWidget(shoptitle)
+rowstyle ="""
+    #Row {
+    background-color: #ffb8ce;
+    color: #ff7aa4;
+    border: 3px solid #ff7aa4;   
+    border-radius: 4px;
+    font-size: 17px;
+    font-weight: bold;
+    }
+    #Row:hover{
+    background-color: #f0adc2;
+    }
+    #Row:pressed{
+    padding-top:2px;
+    background-color: #e6a6b9;
+    color: white;
+    }
+"""
+upgraderow = QHBoxLayout()
+testrow1=QPushButton("Test1")
+testrow1.setObjectName("Row")
+
+testrow2=QPushButton("Test2")
+testrow2.setObjectName("Row")
+
+shopscroller = QScrollArea(shopmainwidget)
+shopscroller.setWidgetResizable(True)
+rowstyle2 ="""
+    #RowScroller {
+    background-color: #ffb8ce;
+    color: #ff7aa4;
+    border: 3px solid #ff7aa4;   
+    border-radius: 4px;
+    font-size: 17px;
+    font-weight: bold;
+    }
+"""
+app.setStyleSheet(rowstyle + rowstyle2)
+shopcontainerstack = QStackedWidget()
+shopcontainerstack.setObjectName("RowScroller")
+
+# 1st page
+firstpage = QWidget()
+firstpagelayout = QVBoxLayout(firstpage)
+for i in range(1, 21):
+    upgrade_button = QPushButton(f"TestUpg{i}\nprice: NaN")
+    upgrade_button.setObjectName("Row")
+    firstpagelayout.addWidget(upgrade_button)
+firstpagelayout.addStretch()
+
+# 2nd page
+secondpage = QWidget()
+secondpagelayout = QVBoxLayout(secondpage)
+placeholder12 = QPushButton("TestUpg3\nprice: NaN", secondpage)
+secondpagelayout.addWidget(placeholder12)
+placeholder22 = QPushButton("TestUpg4\nprice: NaN", secondpage)
+secondpagelayout.addWidget(placeholder22)
+secondpagelayout.addStretch()
+
+shopcontainerstack.addWidget(firstpage)
+shopcontainerstack.addWidget(secondpage)
+
+testrow1.clicked.connect(lambda: shopcontainerstack.setCurrentIndex(0))
+testrow2.clicked.connect(lambda: shopcontainerstack.setCurrentIndex(1))
+
+shopscroller.setWidget(shopcontainerstack)
+shopscroller.resize(shopmainwidget.width(), shopmainwidget.height()-120)
+shopscroller.move(0,120)
 
 
+upgraderow.addWidget(testrow1)
+upgraderow.addWidget(testrow2)
+shopmainlayout.addLayout(upgraderow)
+shopmainlayout.addStretch()
+shopmainlayout.setContentsMargins(0, 0, 0, 0)
+shopmainlayout.setSpacing(2)
 
+
+centerer(shopmainwidget, 755 , 0)
 
 centerer(money, -5, -350)
 
